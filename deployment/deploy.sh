f16b171663d162983432cee5f44cbc01535c7d6f
gavin <g@bitdiff.com>
2020-10-02 12:28:14 +0100

    changed deploy script to use region

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,13 @@
-export REGION=eu-west-2
-export DIST_OUTPUT_BUCKET=build-tools.incentivesmart.com
-export SOLUTION_NAME=ib-sih
-export VERSION=1.0.0
+#!/bin/bash
+
+REGION=eu-west-2
+DIST_OUTPUT_BUCKET=build-tools.incentivesmart.com
+SOLUTION_NAME=ib-sih
+VERSION=1.0.0
 
 ./build-s3-dist.sh $DIST_OUTPUT_BUCKET $SOLUTION_NAME $VERSION
 
-aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
-aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
+echo $DIST_OUTPUT_BUCKET-$REGION/
+
+aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
+aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
